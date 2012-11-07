8a1361b7a19a20c7f5a61ba4e3899219950b4b57
tokumura <tokumura@smc-gitlab.org>
2012-11-07 17:48:54 +0900

    add feature 'mainmenu'. but it's pending yet.

---
+++ features/step_definitions/mainmenu_39099269_steps.rb
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+Given /^遷移先にセットリスト作成と楽曲管理がある。$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^メインメニュー画面が表示された時、$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^Epsteinというタイトルが表示される。$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^セットリスト作成 と 楽曲管理 の２つのリンクが表示される。$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
