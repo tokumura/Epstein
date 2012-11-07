e5406498d647adf752317a1e7155d16286d068f7
tokumura <tokumura@smc-gitlab.org>
2012-11-07 14:43:40 +0900

    create app 'Epstein'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epstein::Application.config.secret_token = '5cd9c79b20e4fb9ce2446a87456037233d22d9f0737962666a837ba385340f3ee160d1a00bff61e4c30003ba16575503bfba87dd2fe4e6d64216433b66236615'
