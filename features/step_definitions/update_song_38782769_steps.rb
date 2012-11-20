f8cde61c2c050c4a50deddbd9f14ad7c906bc66d
tomoya <tomoya@tomoya-pangolin.(none)>
2012-11-20 10:29:29 +0900

    [#38782769] テスト追加

--- features/step_definitions/update_song_38782769_steps.rb
+++ features/step_definitions/update_song_38782769_steps.rb
@@ -11,14 +11,20 @@ When /^ユーザーが楽曲リンクをクリックする、$/ do
 end
 
 Then /^編集フォームにクリックした楽曲情報が表示される。$/ do
-  pending
+  find_field('music[title]').value.should have_content('You Know My Name')
+  find_field('music[vocal]').value.should have_content('John&Paul')
 end
 
-Then /^楽曲情報を変更して、更新ボタンを押すと、$/ do
-  pending # express the regexp above with the code you wish you had
+Then /^楽曲情報を変更して、登録ボタンを押すと、$/ do
+  fill_in 'music[title]', :with => 'You Know My Game'
+  select('John', :form => 'music[vocal]')
+  click_on('登録')
 end
 
 Then /^楽曲情報が更新され、フォームは空になる。$/ do
-  pending # express the regexp above with the code you wish you had
+  find_field('music[title]').value.should_not have_content('You Know My')
+  find_field('music[vocal]').value.should have_content('John')
+  find('#music_list').should_not have_content('You Know My Name')
+  page.click_link('You Know My Game')
 end
 
